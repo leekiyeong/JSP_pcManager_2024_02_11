6b99cef1c74849f95d6b27d5b53e9e8cac1d32f6
leekiyeong <pyocosss@naver.com>
2024-02-11 18:13:52 +0900

    req와 resp를Rq를 이용해 해결

--- src/main/java/com/lky/JSP_pcManager/servlet/GugudanServlet.java
+++ src/main/java/com/lky/JSP_pcManager/servlet/GugudanServlet.java
@@ -1,5 +1,6 @@
 package com.lky.JSP_pcManager.servlet;
 
+import com.lky.JSP_pcManager.Rq;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -12,16 +13,15 @@ import java.io.IOException;
 public class GugudanServlet extends HttpServlet {
   @Override
   protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-    req.setCharacterEncoding("UTF-8");
-    resp.setCharacterEncoding("UTF-8");
-    resp.setContentType("text/html; charset=utf-8");
+    Rq rq = new Rq(req, resp);
 
-    int dan = Integer.parseInt(req.getParameter("dan"));
-    int limit = Integer.parseInt(req.getParameter("limit"));
-    resp.getWriter().append("<h1>%d단</h1>\n".formatted(dan));
+    int dan = rq.getIntParam("dan", 9);
+    int limit = rq.getIntParam("limit", 9);
+
+    rq.appendBody("<h1>%d단</h1>\n".formatted(dan));
 
     for(int i = 1; i <= limit; i++){
-      resp.getWriter().append("<div>%d * %d = %d</div>\n".formatted(dan, i, dan * i));
+      rq.appendBody("<div>%d * %d = %d</div>\n".formatted(dan, i, dan * i));
     }
   }
 }

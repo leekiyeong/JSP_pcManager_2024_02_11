4d3915efb4b806aece62b9a676c9b79ad45380f0
leekiyeong <pyocosss@naver.com>
2024-02-11 15:32:46 +0900

    세팅

---
+++ src/main/java/com/lky/JSP_pcManager/Main.java
@@ -0,0 +1,9 @@
+package com.lky.JSP_pcManager;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("JSP시작");
+  }
+}

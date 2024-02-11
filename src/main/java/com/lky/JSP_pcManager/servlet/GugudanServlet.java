7c5863896e83c81a14ec55871e00a4cda2cb757b
leekiyeong <pyocosss@naver.com>
2024-02-11 22:43:28 +0900

    JSP에 model2방식 적용

--- src/main/java/com/lky/JSP_pcManager/servlet/GugudanServlet.java
+++ src/main/java/com/lky/JSP_pcManager/servlet/GugudanServlet.java
@@ -1,6 +1,7 @@
 package com.lky.JSP_pcManager.servlet;
 
 import com.lky.JSP_pcManager.Rq;
+import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -14,14 +15,17 @@ public class GugudanServlet extends HttpServlet {
   @Override
   protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
     Rq rq = new Rq(req, resp);
-
+    //param 받기 위함
     int dan = rq.getIntParam("dan", 9);
     int limit = rq.getIntParam("limit", 9);
 
-    rq.appendBody("<h1>%d단</h1>\n".formatted(dan));
+    //requset에 정보를 담는다.
+    //gugudan2.jsp에서 해당 내용을 꺼내 쓸 수 있기 때문에.
+    req.setAttribute("dan", dan); //key,value
+    req.setAttribute("limit", limit);
 
-    for(int i = 1; i <= limit; i++){
-      rq.appendBody("<div>%d * %d = %d</div>\n".formatted(dan, i, dan * i));
-    }
+    //gugudan2.jsp에 나머지 작업을 토스
+    RequestDispatcher requestDispatcher = req.getRequestDispatcher("/gugudan2.jsp");
+    requestDispatcher.forward(req, resp);
   }
 }

18ad34a7ec31153d20972a007b5454272d01a3e7
leekiyeong <pyocosss@naver.com>
2024-02-18 18:09:03 +0900

    톰캣 실행시 테스트데이터 10개 생성

--- src/main/java/com/lky/jsp/pcManager/article/ArticleRepository.java
+++ src/main/java/com/lky/jsp/pcManager/article/ArticleRepository.java
@@ -4,6 +4,7 @@ import com.lky.jsp.pcManager.article.dto.ArticleDto;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.IntStream;
 
 public class ArticleRepository {
   private static List<ArticleDto> datum;
@@ -12,8 +13,19 @@ public class ArticleRepository {
   static {
     datum = new ArrayList<>();
     lastId = 0;
+
+    makeTestData();
   }
-  public long write(String title, String body) {
+
+  private static void makeTestData() {
+    IntStream.rangeClosed(1, 10).forEach(id -> {
+      String title = "제목%d".formatted(id);
+      String body = "내용%d".formatted(id);
+      write(title, body);
+    });
+  }
+
+  public static long write(String title, String body) {
 
     long id = ++lastId;
 
@@ -24,11 +36,11 @@ public class ArticleRepository {
     return id;
   }
 
-  public List<ArticleDto> findAll() {
+  public static List<ArticleDto> findAll() {
     return datum;
   }
 
-  public ArticleDto findById(long id) {
+  public static ArticleDto findById(long id) {
     for(ArticleDto articleDto : datum) {
       if(articleDto.getId() == id){
         return articleDto;

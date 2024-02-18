ca6fd212d528e21ad7e420a13c42cc848ef08a62
leekiyeong <pyocosss@naver.com>
2024-02-18 17:23:26 +0900

    게시물 리스트에서 등록된 게시물이 나오도록

--- src/main/java/com/lky/jsp/pcManager/article/ArticleController.java
+++ src/main/java/com/lky/jsp/pcManager/article/ArticleController.java
@@ -13,16 +13,10 @@ public class ArticleController {
     articleService = new ArticleService();
   }
   public void showList(Rq rq) {
+    List<ArticleDto> articleDtos = articleService.findAll();
 
-    List<ArticleDto> articleDtos = new ArrayList<>();
-    for (int i = 5; i >= 1; i--) {
-      articleDtos.add(new ArticleDto(i, "제목" + i, "내용" + i));
-    }
     rq.setAttr("articles", articleDtos);
-    System.out.println("showList set 메서드가 호출 되었습니다.");
-    System.out.println(rq);
     rq.view("usr/article/list");
-    System.out.println("showList view 메서드가 호출 되었습니다.");
   }
 
   public void showWrite(Rq rq) {
